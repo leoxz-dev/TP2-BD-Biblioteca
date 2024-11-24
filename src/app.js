91551b1680f95e48803454931a9de380a1d41c30
AgustinNicolasRG <tuemail@example.com>
2024-11-24 09:53:00 -0300

    Agregue cliente de prisma a app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
+const { PrismaClient }= require('@prisma/client')
 const express = require('express')
 const app = express()
 const port = 3000
+
+const prisma = new PrismaClient()
+
 
 app.use(express.json())
 
